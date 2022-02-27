48236cf5e5ca2bd9d9aeb833f1d0cc30af107c57
ElianMartinezDev <elian.martinez@grupobabel.com>
2022-02-27 13:36:12 -0400

    left and right done funtionality

--- src/components/Carrusel/index.jsx
+++ src/components/Carrusel/index.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
 import { Card } from "../Card";
 import { movies } from "../../data/movies";
-import arrow from "../../assets/arrow.png";
 import styles from "./Carrusel.module.css";
 
 export const Carrusel = () => {
-  console.log(styles);
   const scroll = React.createRef();
   const [moviesArray, setMovies] = useState(movies);
   const [puede, setPuede] = useState(false);
-  const [currentSelect, setCurrentSelect] = useState(2);
+  const [currentSelect, setCurrentSelect] = useState(0);
+
   const [dimensions, setDimensions] = React.useState({
     height: window.innerHeight,
     width: window.innerWidth,
@@ -25,24 +24,58 @@ export const Carrusel = () => {
     window.addEventListener("resize", handleResize);
   }, []);
 
+  const slideRight = React.useCallback(() => {
+    const SIZE_CARD = dimensions.width / 5;
+    if (currentSelect > 0) {
+      scroll.current.scrollLeft -= SIZE_CARD;
+      setCurrentSelect(currentSelect - 1);
+    } else {
+      scroll.current.scrollLeft = scroll.current.scrollWidth;
+      setCurrentSelect(movies.length - 1);
+    }
+  }, [scroll, dimensions.width, currentSelect]);
+
   const slideLeft = React.useCallback(() => {
-    setCurrentSelect(currentSelect + 1);
-  }, [scroll]);
+    const SIZE_CARD = dimensions.width / 5;
+    if (currentSelect < movies.length - 1) {
+      scroll.current.scrollLeft += SIZE_CARD;
+      setCurrentSelect(currentSelect + 1);
+    } else {
+      scroll.current.scrollLeft = 0;
+      setCurrentSelect(0);
+    }
+  }, [scroll, dimensions.width, currentSelect]);
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.scrollContainer} ref={scroll}>
-        {moviesArray.map((item, i) => {
-          return (
-            <Card
-              active={i === currentSelect}
-              width={dimensions.width}
-              key={i}
-              {...item}
-            />
-          );
-        })}
+    <>
+      <div className={styles.wrapper}>
+        <div
+          onClick={slideLeft}
+          className={`${styles.nextButon} ${styles.button}`}
+        >
+          <span>&#10093;</span>
+        </div>
+        <div
+          onClick={slideRight}
+          className={`${styles.backButon} ${styles.button}`}
+        >
+          <span>&#10092;</span>
+        </div>
+        <div className={styles.scrollContainer} ref={scroll}>
+          <div style={{ marginLeft: "100px" }}></div>
+          {moviesArray.map((item, i) => {
+            return (
+              <Card
+                active={i === currentSelect}
+                width={dimensions.width}
+                key={i}
+                {...item}
+              />
+            );
+          })}
+          <div style={{ marginRight: "100px" }}></div>;
+        </div>
       </div>
-    </div>
+    </>
   );
 };
